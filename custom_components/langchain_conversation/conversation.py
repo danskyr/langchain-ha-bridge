3744c2591259903bc27194826a7f91714f99a68f
Jack Coy <jackman3000@gmail.com>
2025-07-07 17:06:58 +1000

    copy over the async_process from `ConversationEntity` to fulfil error
     - ?

--- custom_components/langchain_conversation/conversation.py
+++ custom_components/langchain_conversation/conversation.py
@@ -3,11 +3,13 @@ import logging
 
 import aiohttp
 from homeassistant.components import assist_pipeline, conversation as conversation
-from homeassistant.components.conversation import AbstractConversationAgent, ConversationResult, ConversationEntity
+from homeassistant.components.conversation import AbstractConversationAgent, ConversationResult, ConversationEntity, \
+    async_get_chat_log
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import intent
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.chat_session import async_get_chat_session
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.intent import IntentResponseErrorCode
 
@@ -36,6 +38,31 @@ class RemoteConversationAgent(AbstractConversationAgent, ConversationEntity):
     def supported_languages(self) -> list[str]:
         """Return a list of supported languages."""
         return ["en"]
+
+    # async def async_process(self, user_input: conversation.ConversationInput) -> ConversationResult:
+    #     """Process a sentence."""
+    #     entry_id = list(self.hass.data[DOMAIN].keys())[0]
+    #     url = self.hass.data[DOMAIN][entry_id].get("url", "http://127.0.0.1:8000/process")
+    #
+    #     # fire off HTTP POST to your LangChain server
+    #     resp = requests.post(url, json={"text": text}, timeout=10)
+    #     resp.raise_for_status()
+    #     response_text = resp.json()["response"]
+    #
+    #     return ConversationResult(
+    #         response=response_text,
+    #         conversation_id=conversation_id,
+    #     )
+
+    # Note: This `async_process` is copied straight from `ConversationEntity`,
+    #       but is marked as abstract on `AbstractConversationAgent` and required to be implemented here...
+    async def async_process(self, user_input: conversation.ConversationInput) -> ConversationResult:
+        with (
+            async_get_chat_session(self.hass, user_input.conversation_id) as session,
+            async_get_chat_log(self.hass, session, user_input) as chat_log,
+        ):
+            return await self._async_handle_message(user_input, chat_log)
+
 
     async def _async_handle_message(
             self,
